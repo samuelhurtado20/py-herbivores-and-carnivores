5150082c5397a2dfca923c7934200609a3ddd8cb
Samuel Hurtado <samuelhurtado20@hotmail.com>
2026-03-10 10:38:13 -0500

    Add Animal, Herbivore, and Carnivore classes

--- app/main.py
+++ app/main.py
@@ -1,1 +1,38 @@
+class Animal:
+    Alive = []
+
+    def __init__(self, name: str, health: int = 100, hidden: bool = False):
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.Alive.append(self)
+
+    def hide(self):
+        self.hidden = not self.hidden
+
+    def __repr__(self):
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+    def alive():
+        return [repr(animal) for animal in Animal.Alive]
+
+
+class Herbivore(Animal):
+    def __init__(self, name: str, health: int = 100, hidden: bool = False):
+        super().__init__(name, health, hidden)
+
+    def be_eaten(self, amount: int):
+        if not self.hidden:
+            self.health -= amount
+        if self.health <= 0:
+            Animal.Alive.remove(self)
+
+
+class Carnivore(Animal):
+    def __init__(self, name: str, health: int = 100, hidden: bool = False):
+        super().__init__(name, health, hidden)
+
+    def bite(self, herbivore: Herbivore):
+        if not self.hidden and not herbivore.hidden:
+            herbivore.health -= 50
 # write your code here
